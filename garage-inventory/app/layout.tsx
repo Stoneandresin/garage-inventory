c17d5b74c44bec5cdee73a16161664b1b857b551
Stoneandresin <aaron@stoneandresin.com>
2025-09-07 13:04:39 -0700

    Create layout.tsx

---
+++ garage-inventory/app/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const metadata = {
+  title: "Garage Inventory",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
